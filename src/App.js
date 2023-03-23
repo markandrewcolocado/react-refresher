da6bc9fdf0508764961d8bea0800e574d8520698
Mark Andrew Colocado <mark05@simplexi.com>
2023-03-23 16:58:29 +0800

    routing and components.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { Route, Switch } from "react-router-dom";
+import MainNavigation from "./components/layout/MainNavigation";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupsPage from "./pages/NewMeetup";
+
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupsPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
